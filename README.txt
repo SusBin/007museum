44ba10ff7974a991d9f3903d4cb09093f36b8d18
SusBin <sb2096@hw.ac.uk>
2023-10-18 20:51:34 +0100

    InitialCommit

---
+++ README.txt
@@ -0,0 +1,28 @@
+Main purpose
+============
+
+The main purpose of this site is to attract visitors to the museum.
+
+Users
+=====
+
+As a User I want to know see the location and costs of visiting the museum so that I can plan my visit.
+
+As a user I want to see information about the 007 Museum so that I know what exhibits are there to explore.
+
+Information of the webpage
+==========================
+The main information includes museum’s name, address, location and opening times as well as selected information on exhibits about exciting, interesting & curious aspects of 007 life, and artifacts to attract prospective visitors
+
+The site’s main purpose is to attract visitors to the museum.
+
+
+Justification for any libraries used
+====================================
+Font awsome icon library is a widely used library of icons. I have chosen to use this library to include commonly used icons that I expect most users are familiar with to improve user experience. 
+
+
+URL of the original webpage
+===========================
+
+http://www.007museum.com/
---
+++ script.js
@@ -0,0 +1,129 @@
+let ExhibitionList = [
+    {  
+      "title": "Triumph Scrambler 1200 Bond Edition",  
+      "description": "In December 2019, Triumph announced a new official partnership with EON Productions, revealing the dynamic action feature of Triumph motorcycles in the forthcoming 25th James Bond Film, No Time To Die. To celebrate this iconic collaboration, Triumph, the premium British motorcycle brand, is proud to introduce the first ever official motorcycle directly linked to the Bond Franchise. The Scrambler 1200 Bond Edition is an ultra-rare, limited edition Scrambler 1200 motorcycle featuring a unique 007 design scheme and limited to a production of just 250 models worldwide. This special Bond Edition motorcycle was inspired by the incredible custom Scrambler 1200 action vehicles featured in No Time To Die and has all of the top-specification Triumph Scrambler 1200 XE’s exceptional performance and capability, together with premium higher-value details",  
+      "detail": "<p>In December 2019, Triumph announced a new official partnership with EON Productions, revealing the dynamic action feature of Triumph motorcycles in the forthcoming 25th James Bond Film, No Time To Die.</p><p>To celebrate this iconic collaboration, Triumph, the premium British motorcycle brand, is proud to introduce the first ever official motorcycle directly linked to the Bond Franchise. The Scrambler 1200 Bond Edition is an ultra-rare, limited edition Scrambler 1200 motorcycle featuring a unique 007 design scheme and limited to a production of just 250 models worldwide. This special Bond Edition motorcycle was inspired by the incredible custom Scrambler 1200 action vehicles featured in No Time To Die and has all of the top-specification Triumph Scrambler 1200 XE’s exceptional performance and capability, together with premium higher-value details;</p><h2>Beautiful and unique James Bond design scheme</h2><p>Premium 007 bodywork, including:</p><ul><li>Beautiful exhaust number board and lower side panel finisher with 007 branding.</li><li>Premium real leather seat with embroidered logo.</li><li>Unique Bond TFT instrument startup screen.</li></ul><p>Distinctive 007 paint scheme inspired by the bike ridden by James Bond in the twenty-fifth film. Blacked-out finishes, including:</p><ul><li>Black anodized rear and high-level front mudguard.</li><li>Elegant black forks.</li><li>High-value engine badges with gold accents.</li><li>Black powder coated swingarm and sprocket cover.</li><li>Black anodized grab rail, sump guard and infills.</li><p>Beautiful features and details, including:</p><ul><li>Fog lights with unique black anodized shrouds.</li><li>Arrow silencer with carbon fiber end caps.</li><li>Machined front brake reservoir.</li><li>Black rear wheel adjusters.</li><li>Stainless steel headlight grill.</li></ul><p>Ultra-rare limited edition</p><ul><li>250 worldwide production.</li><li>Each with a numbered edition plaque and unique badging.</li><li>Special Bond handover pack.</li></ul><p>All of the Scrambler 1200 XE exceptional performance & capability</p><ul><li>Thrilling twin-powered performance - 90 PS / 89 BHP @ 7,400 rpm & 110 Nm / 81.1 LB-FT @ 3,950 rpm</li><li>Category-dominating capability</li><li>Class-leading state-of-the-art technology, specification and equipment</li><li>Breathtaking Scrambler style</li>",
+      "imageSource":"images/triumph_no_time_to_die.jpg",
+      "alt" : "No Time To Die Triumph scrambler 1200 Bond edition",
+      "gallery":["images/triumph_no_time_to_die_007-museum.jpg","images/scrambler_1200_bond_edition_nybro.jpg","images/triumph_no_time_to_die_james_bond.jpg","images/triumph_bond_edition_007_nttd.jpg","images/triumph_no_time_to_die_plate.jpg"],
+      "galleryAlt":["No Time to Die movie poster with bond edition motorcycle image", "Bond edition 244 or 250 Scrambler tag", "Gunnar Schäfer with limited Bond edition motorcycle", "Limited Bond edition Triumph morotcycle", "No Time to Die 007 motorcycle number plate"]
+    },  
+    {  
+      "title": "Hovercraft Osprey 5",  
+      "description": "The James Bond film \"Die Another Day\" featured three hovercraft, two Osprey 5s and a Slingsby SAH 2200. One of each craft used in the film can be found on display here at the museum, alongside a multimedia presentation about the filming of the hovercraft chase.",  
+      "detail": "<h2>Die Another Day 2002. Hovercraft chase</h2><p>The tank test tracks at Farnborough became demilitarised Korea for a scene which involved a hovercraft chase at the beginning of \"Die Another Day\" and has appeared in a number of 007 outings and set a world record in Die Another Day for the...</p><p>The Hovercraft Osprey 5 who runs by Pierce Brosnan in the James Bond movie \"Die Another Day\". On display in James Bond 007 Museum Nybro Sweden.</p><iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Cemapv0M6q8\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe><p>Hovercraft Osprey 5 original as Bond Pierce Brosnan drove in the movie \"Die Another Day\" 2002. Having been at The Hovercraft Museum in Southampton, England, since 2002, the Osprey 5 hover has finally found home. It still has a lot to offer, and in the clip above it is tested for the first time on Swedish soil. Warwick Jacobs from Hovercraft Museum in Gosport Hovercraft will be featured at the James Bond Museum in Nybro.</p>",
+      "imageSource":"images/die-another-day-hovercraft.jpg",
+      "alt" : "Pierce Brosnan ex. James Bond Hovercraft",
+      "gallery":["images/osprey_bond1.jpg","images/bond_warwick_hover.jpg","images/007_james_hover.jpg","images/007_bond_hover.jpg","images/sign_die_another_day1.jpg"],
+      "galleryAlt":["Gunnar Schäfer with Hovercraft Osprey 5", "Hovercraft Osprey outside 007 Museum Nybro", "Hovercraft Osprey 5", "Hovercraft Osprey 5", "Artifcacts from Die Another Day"]
+    },  
+    {  
+      "title": "Lotus Esprit S3 Turbo Coupe",  
+      "description": "Among the longest running of post-war sportscars, the Esprit first surfaced as a Giorgetto Giugiaro-designed Ital Design project car at the 1972 Turin Motor Show, the silver prototype having come about following a chance encounter between Giugiaro and Lotus boss Colin Chapman. The car was well received, and following a protracted development programme finally entered production in 1976. The Esprit retained Lotus’s trademark backbone chassis but deviated from previous designs by employing front and rear sub-frames to support the all-independent suspension and engine/gearbox unit. Lotus’s own 2-litre, 16-valve, four-cylinder engine provided the power while the five-speed transaxle was sourced from Citroën. Disc brakes were fitted to all four wheels, the rears being mounted inboard.",
+      "detail": "<p>Among the longest running of post-war sportscars, the Esprit first surfaced as a Giorgetto Giugiaro-designed Ital Design project car at the 1972 Turin Motor Show, the silver prototype having come about following a chance encounter between Giugiaro and Lotus boss Colin Chapman. The car was well received, and following a protracted development programme finally entered production in 1976. The Esprit retained Lotus’s trademark backbone chassis but deviated from previous designs by employing front and rear sub-frames to support the all-independent suspension and engine/gearbox unit. Lotus’s own 2-litre, 16-valve, four-cylinder engine provided the power while the five-speed transaxle was sourced from Citroën. Disc brakes were fitted to all four wheels, the rears being mounted inboard.</p>\n\n<p>Lotus Esprit Turbo 1984 S3 214 hk Right hand drive.<p>\n<p>This is a Colin Chapman Giugiaro designed - collectors item – used in Bond movies. The design is a little bit more edgy than it used to be –during that time. Exterior in Calypso Red and interior in Champagne only 6 made.<p/>\n\n<p>This is also an example where Colin Chapman (founder of Lotus) used the knowledge from building and competing in – Formula One. Every driver loves the car and its agility.</p>\n<p>It was Chapman who developed the concept to build light-weight cars.</p>\n\n<p>There is just a few of them left in a good condition – that´s why the price goes up -continuously - in the UK. (In the US – even more.)</p>\n\n <p>At the beginning of the film, a white Lotus Turbo Esprit was used, while Roger Moore investigated Gonzale's House. 2 bad guys attempted to break into the car, however, the security system activated and blew up the car! Not very clever security device if you ask me. The second car was a copper coloured Turbo Esprit, shown here, featuring a ski rack on the rear louvers. There were two identical copper cars used in the filming. The second being purchased directly from Lotus Cars in 1998. The car below was actually Lotus's 1980 Turbo Esprit prototype and was originally white, but the car was painted copper, so it would show up against the snowy backgrounds in the film. Both cars helped give the Esprit Turbo some much need promotion and increased the profile of the car to the general public.</p>\n\n<p>After the popularity of the white Lotus Esprit S1, in the 1977 James Bond Film, The Spy Who Loved Me, the new Turbo Esprit was featured in the 1981 film, For Your Eyes Only. Where as the Esprit in the Spy Who Loved me was \"an all sing, all dance\" submarine, the Turbo Esprit's took on less ambitious roles in the film.<p>",
+      "imageSource":"images/Lotus_Esprit_Turbo.jpg",
+      "alt" : "1984  Lotus Esprit Turbo Coupe",
+      "gallery":["images/Lotus_Esprit_Turbo_museum.jpg","images/Lotus_Esprit_Turbo007a.jpg","images/Lotus_Esprit_Turbo_007_museum.jpg","images/Lotus_Esprit_Turbo_Bond.jpg","images/Lotus_Esprit_Turbosn.jpg"],
+      "galleryAlt":["Lotus Esprit Turbo 1984 in Calypso red","007 JB number plate on Lotus Esprit", "Lotus Esprit Turbo 1984 in Calypso red outside 007 Museum Nybro", "Lotus Esprit Turbo 1984 in Calypso red inside 007 Museum Nybro", "Copper coloured Lotus Esprit featuring a ski rack on the rear louvers"]
+    },
+    {  
+      "title": "James Bond Museum Gondola",  
+      "description": "Visit the first Gondola in Sweden which is similar to that was used in the Bond film Moonraker. The Gondola was transported from Venice to the James Bond museum in Nybro Sweden.",
+      "detail": "<p>Sweden's first Gondola from Venice to James Bond Museum in Nybro world's only James Bond museum similar to the gondola that was used in the Bond film Moonraker.</p><p>Gunnar Schäfer has built a museum in Nybro dedicated entirely to 007. And in the footsteps of the film \"Moonraker\" wanted to buy a gondola and send it to Sweden Gunnar's gondola from Venice to Sweden La departure of the gondola from Tronchetto, the scene of the gondola in 007 Moonraker. The post entitled: \"James Bond fan, buy a gondola and take it to Sweden\" appeared on the day 24/07/2013, at 17:07, on the online newspaper Nuova Venezia where every day you can find the latest news of the geographical area relative to Venice.</p><p>VENICE. A gondola in Sweden, at the museum dedicated to James Bond. It is realized in recent days the dream of Gunnar Schäfer, the Swede so fond of 007 movies that he changed his name to \"James Bond\" and created a museum in his Nybro, Sweden. And after seeing the scene from the film \"Moonraker\" in which a gondola literally \"motorized\" glides over St. Mark's Square has decided that the typical Venetian boat It should have been part of his collection.</p><p>The \"James Bond museum\" houses 700 square meters lots of objects, cars, motorcycles and memorabilia about the 007 movies, which Gunnar has harvested over many years. His passion for 007 was born from child, after watching one of the many films in the series.</p><p>Looking for a contact, Gunnar befriended Ettore Belgrade, Venetian gondolier and photographer: We organized the everything, a few days ago then, after meeting him in Venice, the We made contact with a gondolier of a well-known family of Venice. And so Gunnar Schäfer fulfilled his dream of bringing a real gondola in Nybro in his museum.</p><p>The arrival of the gondola at museum 007 was awaited by television Swedish who has made a service</p>",
+      "imageSource":"images/wayborn_gondola.jpg",
+      "alt" : "Bond girl Kristina Wayborn in a Gondola from Venice Moonraker",
+      "gallery":["images/007gondol.jpg","images/ettore_bond_gondola1.jpg","images/gondola_tronchetto_load.jpg","images/bond_gondol_nybro1.jpg","images/james_bond_gondola1.jpg"],
+      "galleryAlt":["Gunnar Schäfer with Gondola inside 007 Museum Nybro","Gondolier Ettore teaching Gunnar Schäfer how to guide Gondola in Venice canal", "Gondola on truck to be transported from Venice to Sweden", "Gunnar Schäfer beside gondola being unloaded at 007 Museum Nybro", "Gunnar Schäfer guiding a gondola in Venice canal"]
+    },
+    {  
+      "title": "Omega Bond Exhibition",  
+      "description": "Since GOLDENEYE in 1995, James Bond’s choice of watch has been the OMEGA Seamaster. This special exhibition of 007 watches at the OMEGA can explore over 22 years of James Bond memorabilia and Seamaster watches, including the same models worn in the films and the Limited Edition tributes such as the most recent “Commander’s Watch” 2017.",
+      "detail": "<p>Since GOLDENEYE in 1995, James Bond’s choice of watch has been the OMEGA Seamaster. This exhibition , special exhibition of 007 watches at the OMEGA can explore over 22 years of James Bond memorabilia and Seamaster watches, including the same models worn in the films and the Limited Edition tributes such as the most recent “Commander’s Watch” 2017.</p><p>Omega Skyfall 600 m inThe James Bond 007 Museum in Nybro Sweden<p><ol><li>The Watches of James Bond 1962-2014 Rolex, Seiko, Omega</li><li>Sneak Peek Omega 50years celebration 1962-2012 with 007 James Bond Skyfall</li><li>Omega Seamaster 007</li><li>Celebrating the return of James Bond: The Seamaster Planet Ocean 600M SKYFALL Limited Edition</li><li>Celebrating the new James Bond film:the Seamaster Planet Ocean 600M ”SKYFALL” Limited Edition</li><li>007 OMEGA Seamaster 300 “SPECTRE” Limited Edition in James Bond 007 Museum to watch</li></ol><p>The Omega Seamaster Planet Ocean 600m Co-Axial ”Quantum Of Solace” Limited Edition watch displays many new elements we’ve never seen on a Planet Ocean watch before.</p><p>Omega have got the details for this Bond watch just about perfect. First of all the sapphire crystal domes lens has been laser engraved on its inner surface with a ”Quantum of Solace” decoration.</p><p>The black textured dial has a pattern that echoes the checkering found on the grip panels of James Bond’s pistol of choice, the Walther PPK. The caseback rounds the subtle Bond themes off with an engraved 007 medallion and the individual edition number of the watch.</p>",
+      "imageSource":"images/commander_omega.jpg",
+      "alt" : "The Seamaster Diver 300M Commander’s Watch Limited Edition",
+      "gallery":["images/omega_casino_007.jpg","images/skyfall_showroom.jpg","images/omega_quantum_of_solace.jpg","images/secret-agent-omega-watch-diver-300m-large.jpg","images/omega_spectre_kit2.jpg"],
+      "galleryAlt":["Omega Seamaster Planet Ocean 'Casino Royal' Limited Edition watch", "Omega Seamaster Planet Ocean 600m James Bond 2012 'Skyfall' Limited Edition watch", "Omega Seamaster Planet Ocean 60m Co-Axial 'Quantum Of Solace' Limited Edition watch", "Omega Seamaster Diver 300M 007 Edition 'No Time To Die' Bond watch", "OMEGA Seamaster Aqua Terra 150M James Bond 'Spectre' Limited Edition watch" ]
+    },
+    {  
+      "title": "Little Nellie Autogiro JT3",  
+      "description": "Helicopter Autogiro JT3 looks similar to Little Nellie Gyrocopter from \"You Only Live Twice\" 1967 with Sean Connery as James Bond",
+      "detail": "<p>Little Nellie Autogiro JT3 Nybro Sweden James Bond 007 Museum. Helicopter Autogiro JT3 looks like and simular to Little Nellie Gyrocopter from the movie You Only Live Twice 1967 with Sean Connery as James Bond.</p>",
+      "imageSource":"images/autogiro-jt3_nybro.jpg",
+      "alt":"Helicopter Autogiro JT3",
+      "gallery":["images/gyrocopter-bond.jpg","images/gyrocpter-007.jpg","images/Little_Nelli_wa_116.jpg","images/autogiro-jt3_007.jpg","images/wallis-nellie.jpg"],
+      "galleryAlt":["Gunnar Schäfer with helicopter autogiro JT3 which looks similar to Little Nellie Gyrocopter from 'You only live Twice'", "helicopter autogiro JT3 suspended from support beam in 007 museum, Nybro", "Little Nellie Gyrocopter from 'You only Live Twice'", "helicopter autogiro JT3 outside 007 museum, autogiro-jt3_nybro", "Ken Wallis flying WA-116 (xr-943) Autogyro 'Little Nellie'" ]
+    }  
+  ];
+
+let text = "";
+let currentImage = 0;
+let currentExhibition = 0;
+
+function moreInfo(index) {
+    container.style.display = "none";
+    overlay.style.display = "block";
+    document.getElementById("overlay-header").innerHTML = ExhibitionList[index].title;
+    document.getElementById("overlay-text").innerHTML = ExhibitionList[index].detail;
+    currentExhibition = index;
+    displayImages(index);
+  };
+
+  function displayImages(index) {
+    // Clear the gallery
+    const gallery = document.getElementById("overlay-gallery");
+    gallery.innerHTML = "";
+
+    // Get the current exhibition
+    const exhibition = ExhibitionList[index];
+
+    // Display the images for the current exhibition
+    const img = document.createElement("img");
+    img.src = exhibition.gallery[currentImage];
+    img.alt = exhibition.galleryAlt[currentImage];
+    gallery.appendChild(img);
+  };  
+
+window.addEventListener("load", function() {
+
+  const overlay = document.getElementById("overlay");
+  const closeButton = document.getElementById("close-button");
+  const container = document.getElementById("container");
+  const prevButton = document.querySelector("#prev-button");
+  const nextButton = document.querySelector("#next-button");
+  const exhibitionButton = document.querySelector('#exhibitionButton');
+
+  const populateWebcards = function() {
+    for (var i = 0; i < ExhibitionList.length; i++) {
+      text += '<li><img src="' + ExhibitionList[i].imageSource + '" alt="' + ExhibitionList[i].alt + '">';
+      text += "<h3>" + ExhibitionList[i].title + "</h3>";
+      text += "<p>" + ExhibitionList[i].description + "</p>";
+      text += '<a href="#" onclick="moreInfo(' + i + ')">' + ExhibitionList[i].title + '<i class="fas fa-angle-double-right"></i></a></li>';
+    }
+    document.getElementById("exhibitionsList").innerHTML = text;
+  };
+  populateWebcards();
+
+ closeButton.addEventListener("click", () => {
+    overlay.style.display = "none";
+    container.style.display = "grid";
+  });
+
+  prevButton.addEventListener("click", () => {
+    if (currentImage > 0) {
+      currentImage--;
+      displayImages(currentExhibition);
+    }
+  });
+
+  nextButton.addEventListener("click", () => {
+    if (currentImage < ExhibitionList[currentExhibition].gallery.length - 1) {
+      currentImage++;
+      displayImages(currentExhibition);
+    }
+  });
+
+  exhibitionButton.addEventListener('click', function() {
+  document.getElementById('exhibitions').scrollIntoView({ behavior: 'smooth' });
+});
+  
+});
